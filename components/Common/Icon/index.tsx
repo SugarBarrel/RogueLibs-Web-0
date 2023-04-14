c047f6e8fe25cce5c1a6dd155d210f52cf302e31
Abbysssal <hillankeyxl2@yandex.ru>
2023-04-15 02:17:04 +0700

    ♻️ Refactor icons

--- components/Common/Icon/index.tsx
+++ components/Common/Icon/index.tsx
@@ -1,53 +1,37 @@
-import Sprite, { AnimatedSprite, AnimatedSpriteProps, SpriteProps } from "../Sprite";
+import Sprite, { SpriteProps } from "../Sprite";
 
 export type IconProps = Omit<SpriteProps, "src"> & {
   type: IconType;
-  animated?: boolean;
 };
-export default function Icon({ className, type, animated, ...props }: IconProps) {
-  if (type in animatedIconTypes) {
-    const anim = animatedIconTypes[type]!;
-    if (animated === false) {
-      return <Sprite className={className} src={anim[0][0]} crisp {...props} />;
-    }
-    return <AnimatedSprite className={className} src={anim[0]} crisp interval={anim[1]} {...props} />;
-  }
-  return <Sprite className={className} src={`/icons/${type}.png`} crisp {...props} />;
+export default function Icon({ type, ...props }: IconProps) {
+  return <Sprite src={IconPaths[type]} crisp {...props} />;
 }
+
+export const IconPaths = {
+  loading: "/icons/loading.gif",
+  check: "/icons/check.png",
+  cross: "/icons/cross.png",
+  eye: "/icons/eye.png",
+  nugget: "/icons/nugget.png",
+  upload: "/icons/upload.png",
+  download: "/icons/download.png",
+  discord: "/icons/discord.png",
+  copy: "/icons/copy.png",
+  edit: "/icons/edit.png",
+  save: "/icons/save.png",
+  link: "/icons/link.png",
+} as const;
+
+export type IconType = keyof typeof IconPaths;
 
 export function MakeIcon(type: IconType) {
+  const src = IconPaths[type];
   return function Icon(props: Omit<SpriteProps, "src">) {
-    return <Sprite src={`/icons/${type}.png`} crisp {...props} />;
-  };
-}
-export function MakeAnimatedIcon(type: IconType, frameCount: number, interval: number) {
-  const srcs = Array.from({ length: frameCount }).map((_, i) => `/icons/${type}${i + 1}.png`);
-  animatedIconTypes[type] = [srcs, interval];
-  return function AnimatedIcon(props: Omit<AnimatedSpriteProps, "src" | "interval"> | { interval?: number }) {
-    return <AnimatedSprite src={srcs} interval={interval} crisp {...props} />;
+    return <Sprite src={src} crisp {...props} />;
   };
 }
 
-export const AllIconTypes = [
-  "loading",
-  "check",
-  "cross",
-  "eye",
-  "nugget",
-  "upload",
-  "download",
-  "discord",
-  "copy",
-  "edit",
-  "save",
-  "link",
-] as const;
-
-export type IconType = (typeof AllIconTypes)[number];
-
-const animatedIconTypes: Partial<Record<IconType, [srcs: string[], interval: number]>> = {};
-
-export const LoadingIcon = MakeAnimatedIcon("loading", 4, 50);
+export const LoadingIcon = MakeIcon("loading");
 export const CheckIcon = MakeIcon("check");
 export const CrossIcon = MakeIcon("cross");
 export const EyeIcon = MakeIcon("eye");
