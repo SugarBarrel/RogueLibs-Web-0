580a6b0e577bd4658575d00ecaae26f9cec99900
Abbysssal <hillankeyxl2@yandex.ru>
2023-04-20 16:23:19 +0700

    ✨ Add release visibility checkbox

--- components/ReleasePage/Sidebar.tsx
+++ components/ReleasePage/Sidebar.tsx
@@ -1,8 +1,9 @@
+import { Checkbox } from "@components/Common";
 import { AuthorsList, DownloadsList } from "@components/Editor";
 import { ImmerStateSetter, useImmerSlice } from "@lib/hooks";
+import { DbModAuthor, DbReleaseAuthor } from "@lib/Database";
 import { useReleasePageContext } from ".";
 import styles from "./Sidebar.module.scss";
-import { DbModAuthor, DbReleaseAuthor } from "@lib/Database";
 
 export default function ReleasePageSidebar() {
   const { release, mutateRelease, isEditing, hasChanges } = useReleasePageContext();
@@ -12,7 +13,13 @@ export default function ReleasePageSidebar() {
 
   return (
     <div className={styles.sidebar}>
-      <div>{`The release is ${release.is_public ? "public" : "private"}`}</div>
+      <div>
+        {isEditing && (
+          <Checkbox value={release.is_public} onChange={v => mutateRelease(r => void (r.is_public = v))}>
+            {`${release.is_public ? "Public" : "Private"} release`}
+          </Checkbox>
+        )}
+      </div>
 
       <div className={styles.container}>
         <label>{release.files.length === 1 ? "Download" : "Downloads"}</label>
