dad152c97cac4aa8d8f3baa605ef293b9eceb827
Abbysssal <hillankeyxl2@yandex.ru>
2023-04-23 15:30:16 +0700

    ♻️ Refactor user page favourites section

--- components/UserPage/Favourites.tsx
+++ components/UserPage/Favourites.tsx
@@ -4,11 +4,22 @@ import { useState } from "react";
 import { RestUser } from "@lib/API";
 import { upsertUser } from "@ducks/users";
 import { useRootDispatch } from "@ducks/index";
-import styles from "./Favourites.module.scss";
 import { DbUser } from "@lib/Database";
+import styles from "./Favourites.module.scss";
 
 export default function UserPageFavourites() {
-  const { user, mutateUser, canEdit } = useUserPageContext();
+  const { canEdit } = useUserPageContext();
+
+  return (
+    <div className={styles.wrapper}>
+      {canEdit && <PublicNuggetsCheckbox />}
+      <NuggettedModsList />
+    </div>
+  );
+}
+
+export function PublicNuggetsCheckbox() {
+  const { user, mutateUser } = useUserPageContext();
 
   const dispatch = useRootDispatch();
   const [savingPublicNuggets, setSavingPublicNuggets] = useState(false);
@@ -41,20 +52,21 @@ export default function UserPageFavourites() {
   }
 
   return (
-    <div className={styles.wrapper}>
-      {canEdit && (
-        <div className={styles.publicNuggetsCheckbox}>
-          <Checkbox
-            checked={getCheckboxIcon}
-            unchecked={getCheckboxIcon}
-            value={user.public_nuggets}
-            onChange={changePublicNuggets}
-            disabled={savingPublicNuggets}
-          >
-            {user.public_nuggets ? "Everyone can see my nuggets" : "Only I can see my nuggets"}
-          </Checkbox>
-        </div>
-      )}
+    <div className={styles.publicNuggetsCheckbox}>
+      <Checkbox
+        checked={getCheckboxIcon}
+        unchecked={getCheckboxIcon}
+        value={user.public_nuggets}
+        onChange={changePublicNuggets}
+        disabled={savingPublicNuggets}
+      >
+        {user.public_nuggets ? "Everyone can see my nuggets" : "Only I can see my nuggets"}
+      </Checkbox>
     </div>
   );
+}
+export function NuggettedModsList() {
+  const { user } = useUserPageContext();
+
+  return <div></div>;
 }
